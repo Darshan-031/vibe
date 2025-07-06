8f369b7e3c6001a3e611ba9cdded4d61cf1e862b
Darshan <darshanudaiwal90.com>
2025-07-06 14:07:33 +0530

    05: AI jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,12 +1,23 @@
-import { inngest } from "./client";
+import {gemini,createAgent,} from "@inngest/agent-kit";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("Downloading video", "10s");
-    await step.sleep("transcript", "5s");
-    await step.sleep("generating summary", "4s");
-    return { message: `Hello ${event.data.email}!` };
+import {inngest} from "./client";
+
+export const helloWorld =  inngest.createFunction(
+  { id: "summarize-contents" },
+  { event: "app/ticket.created" },
+  async ({ event }) => {
+    const writer = createAgent({
+      name: "summarize",
+      system: "You are an expert summarizer. You can summarize in 2 words",
+
+      model: gemini({ model: "gemini-2.0-flash" }),
+    });
+
+    // Run the agent with an input prompt.
+    const { output } = await writer.run(`summarize the text : ${event.data.value}`);
+
+    console.log(output);
+
+    return output;
   }
 );
