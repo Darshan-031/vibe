f6197bfda52224e10cee768ae9ff64d0b5e04061
Darshan <darshanudaiwal90.com>
2025-07-07 22:36:55 +0530

    07: agent tools

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,6 +1,23 @@
 import { Sandbox }from "@e2b/code-interpreter"
+import { AgentResult, TextMessage } from "@inngest/agent-kit";
 
 export async function getSandbox(sandboxID : string){
     const sandbox = await Sandbox.connect(sandboxID);
     return sandbox;
+}
+
+export function lastAssistentTextMessageContent(result : AgentResult){
+    const lastAssistentTextMessageIndex = result.output.findLastIndex(
+        (message) => message.role === "assistant",
+    );
+
+    const message = result.output[lastAssistentTextMessageIndex] as 
+    | TextMessage 
+    | undefined;
+
+    return message ?.content 
+        ? typeof message.content === "string" 
+            ? message.content 
+            : message.content.map((c) => c.text).join("")
+        : undefined;
 }
