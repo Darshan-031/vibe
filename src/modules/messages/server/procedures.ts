4395f08dce644ca5227490ae6ecab1032563101e
Darshan <darshanudaiwal90.com>
2025-07-08 22:21:00 +0530

    09: projects

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -4,36 +4,41 @@ import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import {z} from "zod";
 
 export const messageRouter = createTRPCRouter({
-    getMany : baseProcedure
-        .query(async () => {
-            const messages = await prisma.message.findMany({
-                orderBy : {
-                    updatedAt : "desc",
-                }
-            })
-            return messages;
-        }),
-    create : baseProcedure
-        .input(
-            z.object({
-                value : z.string().min(1, { message : "message is required "}),
-            })
-        )
-        .mutation(async ({ input }) => {
-            const createdMessage = await prisma.message.create({
-                data : {
-                    content : input.value,
-                    role : "USER",
-                    type : "RESULT",
-                },
-            });
-            await inngest.send({
-              name: "code-agent/run",
-              data: {
-                value: input.value,
-              },
-            });
+  getMany: baseProcedure.query(async () => {
+    const messages = await prisma.message.findMany({
+      orderBy: {
+        updatedAt: "desc",
+      },
+    });
+    return messages;
+  }),
+  create: baseProcedure
+    .input(
+      z.object({
+        value: z
+          .string()
+          .min(1, { message: "Value is required " })
+          .max(10000, { message: "Value is too long" }),
+        projectId: z.string().min(1, { message: "Project ID is required " }),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const createdMessage = await prisma.message.create({
+        data: {
+          projectId : input.projectId,
+          content: input.value,
+          role: "USER",
+          type: "RESULT",
+        },
+      });
+      await inngest.send({
+        name: "code-agent/run",
+        data: {
+          value: input.value,
+          prijectId : input.projectId,
+        },
+      });
 
-            return createdMessage;
-        })
-})
+      return createdMessage;
+    }),
+});
